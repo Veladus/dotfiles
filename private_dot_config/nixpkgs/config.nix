df16217e029f26ba631822ec86dd6fd939da0591
Niko Hastrich <niko.hastrich@student.hpi.uni-potsdam.de>
2021-07-22 19:06:51 +0200

    reswitch to nix

---
+++ private_dot_config/nixpkgs/config.nix
@@ -0,0 +1,29 @@
+{
+  allowUnfree = true;
+  packageOverrides = pkgs: with pkgs; rec {
+    myProfile = writeText "my-profile" ''
+      export PATH=$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH
+      export MANPATH=$HOME/.nix-profile/share/man:/nix/var/nix/profiles/default/share/man:$PATH
+    '';
+    myPackages = pkgs.buildEnv {
+      name = "my-packages";
+      paths = [
+        (runCommand "profile" {} ''
+          mkdir -p $out/etc/profile.d
+          cp ${myProfile} $out/etc/profile.d/my-profile.sh
+        '')
+	chezmoi
+	discord
+	htop
+	keepassxc
+	neovim
+	rustup
+	slack
+	tdesktop
+	thunderbird
+	vscode
+      ];
+    };
+  };
+}
+
