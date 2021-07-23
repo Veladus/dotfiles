27859641760f9ae7c46c80cca95cea69e01441a0
Niko Hastrich <niko.hastrich@student.hpi.uni-potsdam.de>
2021-07-23 14:59:06 +0200

    install sshpass

--- private_dot_config/nixpkgs/config.nix
+++ private_dot_config/nixpkgs/config.nix
@@ -5,6 +5,20 @@
       export PATH=$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH
       export MANPATH=$HOME/.nix-profile/share/man:/nix/var/nix/profiles/default/share/man:$PATH
     '';
+	myNeovim = neovim.override {
+      configure = {
+        packages.myVimPackage = with pkgs.vimPlugins; {
+          start = [
+			delimitMate
+			ultisnips
+			vim-fugitive
+			vim-tmux-navigator
+		    ale
+            vimtex
+		  ];
+        };      
+	  };
+    };
     myPackages = pkgs.buildEnv {
       name = "my-packages";
       paths = [
@@ -16,10 +30,11 @@
 	discord
 	htop
 	keepassxc
-	neovim
 	rustup
 	slack
+	sshpass
 	tdesktop
+	(texlive.combine { inherit (texlive) scheme-medium xpatch framed enumitem mathabx; })
 	thunderbird
 	vscode
       ];
