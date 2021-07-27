90e406bd4e22bfc291f70cbaa7d638d55e85e8d7
Niko Hastrich <niko.hastrich@student.hpi.uni-potsdam.de>
2021-07-27 18:06:43 +0200

    fix manpages

--- private_dot_config/nixpkgs/myRepo/default.nix
+++ private_dot_config/nixpkgs/myRepo/default.nix
@@ -4,15 +4,14 @@
 
 let
     myProfile = pkgs.writeText "my-profile" ''
-      export PATH=$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH
-      export MANPATH=$HOME/.nix-profile/share/man:/nix/var/nix/profiles/default/share/man:$PATH
+      export MANPATH=":"
     '';
 
     myNeovim = import ./neovim {};
 in {
     setupEnv = (pkgs.runCommand "profile" {} ''
           mkdir -p $out/etc/profile.d
-          cp ${myProfile} $out/etc/profile.d/my-profile.sh
+          cp ${myProfile} $out/etc/profile.d/00-my-profile.sh
     '');
 
     inherit myNeovim;
