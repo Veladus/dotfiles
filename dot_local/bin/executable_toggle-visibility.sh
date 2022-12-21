d3bd5025bbcabad70d80aeb446df381fa47aba40
Niko Hastrich <niko.hastrich@student.hpi.uni-potsdam.de>
2022-12-21 10:28:06 +0100

    Fix togglevisibility script for multiple windows

--- dot_local/bin/executable_toggle-visibility.sh
+++ dot_local/bin/executable_toggle-visibility.sh
@@ -4,22 +4,21 @@ export NAME=$1
 
 # this process and grep use name, so look if there is at least one other process
 # Also this process is counted twice
-[ "$(ps -x | grep -c $NAME)" -gt 3 ]
+pidof $NAME
 STARTED=$?
-echo $STARTED $(ps -x | grep $NAME)
 
 if [ "$STARTED" -ne "0" ]; then
 	echo starting
 	$NAME & sleep 0.2
 fi
 
-NODE_ID=$(bspc query -N -n .leaf | xargs -i sh -c 'bspc query --node {} -T | grep -q $NAME && echo {}')
-if [ -z "$NODE_ID" ]; then
-	exit
-fi
-bspc node $NODE_ID --flag hidden
+NODE_IDS=$(bspc query -N -n .leaf | xargs -i sh -c 'bspc query --node {} -T | grep -q $NAME && echo {}')
 
-HIDDEN=$(bspc query -T -n $NODE_ID | jq '.hidden')
-if [ "$HIDDEN" == "false" ]; then
-	bspc node $NODE_ID -f
-fi
+for NODE_ID in ${NODE_IDS[@]}; do
+	bspc node $NODE_ID --flag hidden
+
+	HIDDEN=$(bspc query -T -n $NODE_ID | jq '.hidden')
+	if [ "$HIDDEN" == "false" ]; then
+		bspc node $NODE_ID -f
+	fi
+done
