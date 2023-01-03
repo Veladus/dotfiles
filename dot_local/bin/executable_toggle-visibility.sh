dc9598d8566269cff1eaa3f39218fee6acee541d
Niko Hastrich <niko.hastrich@student.hpi.uni-potsdam.de>
2023-01-03 10:38:33 +0100

    Visibility for keepassxc

--- dot_local/bin/executable_toggle-visibility.sh
+++ dot_local/bin/executable_toggle-visibility.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 export NAME=$1
+export CLASS=$2
+
+function enable_rule() {
+	bspc rule -a $1 state=floating sticky=on hidden=on private=on
+}
+function disable_rule() {
+	bspc rule -r $1
+}
 
 # this process and grep use name, so look if there is at least one other process
 # Also this process is counted twice
@@ -9,6 +17,7 @@ STARTED=$?
 
 if [ "$STARTED" -ne "0" ]; then
 	echo starting
+	enable_rule $CLASS
 	$NAME & sleep 0.2
 fi
 
@@ -18,7 +27,10 @@ for NODE_ID in ${NODE_IDS[@]}; do
 	bspc node $NODE_ID --flag hidden
 
 	HIDDEN=$(bspc query -T -n $NODE_ID | jq '.hidden')
-	if [ "$HIDDEN" == "false" ]; then
+	if [ "$HIDDEN" == "true" ]; then
+		enable_rule $CLASS
+	else
 		bspc node $NODE_ID -f
+		disable_rule $CLASS
 	fi
 done
