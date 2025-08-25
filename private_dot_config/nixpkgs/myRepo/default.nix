f468e9001e64f40cc8960a6435b5191f4aa04629
Niko Hastrich <niko.hastrich@cs.uni-saarland.de>
2025-08-26 01:09:44 +0200

    Format, install vscode, remove kde programs

--- private_dot_config/nixpkgs/myRepo/default.nix
+++ private_dot_config/nixpkgs/myRepo/default.nix
@@ -1,135 +1,158 @@
 {
-    pkgs ? import <nixpkgs> {},
-    pkgs-unstable ? import <nixpkgs-unstable> {},
+  pkgs ? import <nixpkgs> { },
+  pkgs-unstable ? import <nixpkgs-unstable> { },
 }:
 
 let
-    myProfile = pkgs.writeText "my-profile" ''
-      export MANPATH=":"
-      export PATH="$PATH:$HOME/.cargo/bin"
-      export CPATH="$CPATH:$HOME/.nix-profile/include"
-      export LIBRARY_PATH="$LIBRARY_PATH:$HOME/.nix-profile/lib"
-      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.nix-profile/lib"
-    '';
+  myProfile = pkgs.writeText "my-profile" ''
+    export MANPATH=":"
+    export PATH="$PATH:$HOME/.cargo/bin"
+    export CPATH="$CPATH:$HOME/.nix-profile/include"
+    export LIBRARY_PATH="$LIBRARY_PATH:$HOME/.nix-profile/lib"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.nix-profile/lib"
+  '';
 
-    myNeovim = pkgs.callPackage ./neovim {
-      libclang = pkgs.llvmPackages_12.libclang;
-    };
+  myNeovim = pkgs.callPackage ./neovim {
+    libclang = pkgs.llvmPackages_12.libclang;
+  };
 
-    bapc-tools = pkgs.callPackage ./bapc.nix {};
+  bapc-tools = pkgs.callPackage ./bapc.nix { };
 
-#    myTexlive = (pkgs.texlive.combine {
-#      inherit (pkgs.texlive)
-#        adjustbox
-#        algorithm2e
-#        biber
-#        biblatex
-#        blindtext
-#        booktabs
-#        capt-of
-#        caption
-#        cleveref
-#        collectbox
-#        collection-fontsextra
-#        collection-fontsrecommended
-#        collection-langgerman
-#        comment
-#        complexity
-#        csquotes
-#        datetime2
-#        enumitem
-#        forloop
-#        ifoddpage
-#        koma-script
-#        l3kernel
-#        l3packages
-#        latexmk
-#        marginnote
-#        mathtools
-#        mdwtools
-#        microtype
-#        multirow
-#        ncctools
-#        pdfcomment
-#        pdflscape
-#        pgf
-#        pgfplots
-#        relsize
-#        scheme-medium
-#        setspace
-#        siunitx
-#        soul
-#        soulutf8
-#        soulpos
-#        thmtools
-#        threeparttable
-#        tkz-base
-#        todonotes
-#        urlbst
-#        wrapfig
-#        xcolor
-#        xpatch
-#        xstring
-#        zref;
-#    });
-    myTexlive = pkgs.texlive.combined.scheme-full;
+  #    myTexlive = (pkgs.texlive.combine {
+  #      inherit (pkgs.texlive)
+  #        adjustbox
+  #        algorithm2e
+  #        biber
+  #        biblatex
+  #        blindtext
+  #        booktabs
+  #        capt-of
+  #        caption
+  #        cleveref
+  #        collectbox
+  #        collection-fontsextra
+  #        collection-fontsrecommended
+  #        collection-langgerman
+  #        comment
+  #        complexity
+  #        csquotes
+  #        datetime2
+  #        enumitem
+  #        forloop
+  #        ifoddpage
+  #        koma-script
+  #        l3kernel
+  #        l3packages
+  #        latexmk
+  #        marginnote
+  #        mathtools
+  #        mdwtools
+  #        microtype
+  #        multirow
+  #        ncctools
+  #        pdfcomment
+  #        pdflscape
+  #        pgf
+  #        pgfplots
+  #        relsize
+  #        scheme-medium
+  #        setspace
+  #        siunitx
+  #        soul
+  #        soulutf8
+  #        soulpos
+  #        thmtools
+  #        threeparttable
+  #        tkz-base
+  #        todonotes
+  #        urlbst
+  #        wrapfig
+  #        xcolor
+  #        xpatch
+  #        xstring
+  #        zref;
+  #    });
+  myTexlive = pkgs.texlive.combined.scheme-full;
 
-    myIpe = pkgs.qt6Packages.callPackage ./ipe.nix {
-      lua5 = pkgs.lua5_4_compat;
-      texliveSmall = myTexlive;
-    };
+#  myIpe = pkgs.qt6Packages.callPackage ./ipe.nix {
+#    lua5 = pkgs.lua5_4_compat;
+#    texliveSmall = myTexlive;
+#  };
 
-    myGurobi = pkgs.callPackage ./gurobi.nix {};
-in {
-    setupEnv = (pkgs.runCommand "profile" {} ''
-          mkdir -p $out/etc/profile.d
-          cp ${myProfile} $out/etc/profile.d/00-my-profile.sh
-    '');
+  myGurobi = pkgs.callPackage ./gurobi.nix { };
 
-    inherit
-      bapc-tools
-      myIpe
-      myNeovim
-      myTexlive;
+  myVsCode = pkgs.vscode-with-extensions.override {
+    vscodeExtensions =
+      with pkgs.vscode-extensions;
+      [
+        asvetliakov.vscode-neovim
+        tamasfe.even-better-toml
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "lean4";
+          publisher = "leanprover";
+          version = "0.0.211";
+          sha256 = "BoziDTMxM2ffel/3/lD9zf0M7//li1XRcuAUgnbS2o4=";
+        }
+      ];
+  };
+in
+{
+  setupEnv = (
+    pkgs.runCommand "profile" { } ''
+      mkdir -p $out/etc/profile.d
+      cp ${myProfile} $out/etc/profile.d/00-my-profile.sh
+    ''
+  );
 
-    inherit (pkgs.jetbrains) clion;
+  inherit
+    bapc-tools
+    # myIpe
+    myNeovim
+    myTexlive
+    myVsCode
+    ;
 
-    inherit (pkgs.kdePackages) okular;
+  inherit (pkgs.jetbrains) clion;
 
-    inherit (pkgs)
-        ccls
-        chezmoi
-        cmake
-        dejavu_fonts
-        dotool
-        emacs
-        entr
-        evince
-        fd
-        gcc14
-        gdb
-        graphviz
-        htop
-        kdotool
-        keepassxc
-        ninja
-        nix-index
-        nodejs_22
-        proton-pass
-        protonvpn-gui
-        ripgrep
-        rustup
-        signal-desktop
-        slack
-        sshpass
-        tealdeer
-        thunderbird
-        tree
-        wl-clipboard
-        zotero;
+  inherit (pkgs)
+    ccls
+    chezmoi
+    cmake
+    dejavu_fonts
+    dotool
+    elan
+    emacs
+    entr
+    evince
+    fd
+    gcc14
+    gdb
+    graphviz
+    htop
+    kdotool
+    keepassxc
+    ninja
+    nix-index
+    nixfmt-rfc-style
+    nodejs_22
+    proton-pass
+    protonvpn-gui
+    ripgrep
+    rustup
+    signal-desktop
+    slack
+    sshpass
+    tealdeer
+    thunderbird
+    tree
+    wl-clipboard
+    zotero
+    ;
 
-
-    inherit (pkgs-unstable)
-        zig
-        zls;
+  inherit (pkgs-unstable)
+    zig
+    zls
+    ;
 }
